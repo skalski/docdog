74f827c864c02211d0d9d3ef731c7480c48363e6
Swen Kalski <kalski.swen+kernel.dev@gmail.com>
2021-04-17 08:38:32 +0200

    remove unused

--- src/docdog.go
+++ src/docdog.go
@@ -23,7 +23,6 @@ var verbose *bool
 var tabset *int
 
 var objectList []notations.Objects
-var paramsList []notations.Params
 var endpointList []notations.Endpoint
 var tempEndpointList []notations.TempEndpoint
 
